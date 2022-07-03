6ec1f23f9a7e76e7b5bb12a982f07a952afb70fb
Vasile-Hij <vasile.hijs@gmail.com>
2022-07-03 22:02:58 +0300

    1_update

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-## Light and Dark Mode
+# Light and Dark Mode
+The project consists in a static webpage that have been implemented the light/dark switch mode; additionally, navigation bar, buttons, images and a contact page are added to display the functionality of switching light/dark mode.
 
-The project consists in a webpage that have been implemented the light/dark switch mode; additionally, navigation bar, buttons, images and a contact page are added to display the functionality of switching light/dark mode. 
+## Stack
+ - javascript 
 
-Local storage on browser is used to retain the user option, so it will be used by default when the page it will be open next time.
+## Features
+- toggle dark/light mode
+- infinite scroll
+- local storage on browser is used to retain the user option, so it will be used by default when the page it will be open next time. 
+
+## Extra
+ - adaptive for any browser
--- script.js
+++ script.js
@@ -4,50 +4,58 @@ const toggleIcon = document.getElementById('toggle-icon');
 const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
+const image4 = document.getElementById('image4');
 const textBox = document.getElementById('text-box');
-const DARK_THEME = 'dark';
-const LIGHT_THEME = 'light';
 
 // Dark or Light Images
 function imageMode(color) {
-    image1.src = `img/undraw_proud_coder_${color}.svg`;
-    image2.src = `img/undraw_feeling_proud_${color}.svg`;
-    image3.src = `img/undraw_conceptual_idea_${color}.svg`;    
+  image1.src = `img/weather_${color}.svg`;
+  image2.src = `img/charts_${color}.svg`;
+  image3.src = `img/light-dark_${color}.svg`;
+  image4.src = `img/quiz_${color}.svg`;
 }
 
-function toggleDarkLightMode(isDark) {
-    nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
-    textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
-    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
-    isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : 
-        toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');  
-    isDark ? imageMode('dark') : imageMode('light');
+// Dark Mode Styles
+function darkMode() {
+  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  toggleIcon.children[0].textContent = 'DARK MODE';
+  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+  imageMode('dark');
 }
 
+// Light Mode Styles
+function lightMode() {
+  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  toggleIcon.children[0].textContent = 'LIGHT MODE';
+  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  imageMode('light');
+}
 
 // Switch Theme Dynamically
 function switchTheme(event) {
-    if (event.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark')
-        toggleDarkLightMode('dark');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light')
-        toggleDarkLightMode('light');
-    }
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+    darkMode();
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+    lightMode();
+  }
 }
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
 
-//Check Local Storage For Theme
+// Check Local Storage For Theme
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
+  document.documentElement.setAttribute('data-theme', currentTheme);
 
-    if(currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-        toggleDarkLightMode('dark');
-    }
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
 }
